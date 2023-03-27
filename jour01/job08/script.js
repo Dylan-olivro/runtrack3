61828034c5c16987249ba83be48f540b459d02c0
dylan-olivro <dylan.olivro@laplateforme.io>
2023-03-27 13:24:04 +0200

    fixe bug job07

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -6,22 +6,18 @@ function sommenombrepremier(a, b) {
 
   if (a > 1 && b > 1) {
     for (i = 2; i < a, i < b; i++) {
-      if (a % i == 0 && b % i == 0) {
+      if (a % i == 0 || b % i == 0) {
         isPrime = false;
-        break;
+        return "L'un des nombres n'est pas premier.";
       }
     }
-    if (isPrime == true) {
-      console.log(a + b);
+    if (isPrime) {
       return a + b;
-    } else {
-      console.log("L'un des nombres n'est pas premier.");
-      return false;
     }
   }
 }
 
-sommenombrepremier(4, 11);
+console.log(sommenombrepremier(11,4));
 
 //   for (var i = 2; i < nbr; i++) {
 //     if (nbr % i === 0) return false;
